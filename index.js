36f42075ad3b68fc7ac7175ee19798fb1478ee9c
heyjaja <imjaja09@gmail.com>
2022-07-21 01:47:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const dotenv = require("dotenv");
+const dotenv = require("dotenv");// mongoDB URL을 감추기 위해 사용
 dotenv.config();
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT // localhost:port
+const port = process.env.PORT // localhost:[port]
 
 const mongoose = require('mongoose')// mogoose: nodejs와 mongoDB 연결하는 ODM
 mongoose.connect(process.env.MONGO_URL)
