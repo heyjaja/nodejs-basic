8d0a8592e66f072c1e4dad1956e25b2fed27c6f7
heyjaja <imjaja09@gmail.com>
2022-07-30 17:15:15 +0900

    react-router-dom

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />   
+      </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>
+      LandingPage 방구뽕
+    </div>
+  )
+}
+
+export default LandingPage
